20d4863867a1e183fe0ac18dd8d0e8c75d8e4a88
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-01 23:05:58 -0800

    more on notes.txt

--- excercise/threads.js
+++ excercise/threads.js
@@ -1,7 +1,18 @@
+//Testing single-thread performance in Node
 
 const crypto = require('crypto');
 
+const start = Date.now();
+
 crypto.pbkdf2('a','b',100000,512,'sha512',() => {
-  
+  console.log('1:', Date.now() - start);
 });
+
+crypto.pbkdf2('a','b',100000,512,'sha512',() => {
+  console.log('2:', Date.now() - start);
+});
+/*
+
+*/
+
 //EOF
