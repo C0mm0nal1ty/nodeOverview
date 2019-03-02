b2a6c99639c290c0f490352bbd4d61637a870a9c
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-01 20:38:01 -0800

    working on how node uses v8 and libuv.

---
+++ excercise/practice1.js
@@ -0,0 +1,7 @@
+/*
+HOW NODE LEVERAGES V8 AND LIBUV LIBRARY 
+
+pick a function in node standard libary
+find where its implemented in the node soure code
+see how v8 and libuv are used to implement that function
+*/
