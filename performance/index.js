60f2f4dd65e297015e866424a4a5151d9aa51198
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-03-03 00:35:22 -0800

    created a cluster in index.js

--- performance/index.js
+++ performance/index.js
@@ -1,10 +1,33 @@
 //Creating a tiny express server.
 
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-app.get('/', (req,res) => {
-  res.send('Hi there.');
-});
+//first run, determine if file is being executed in master mode?
+//
+if(cluster.isMaster){
+  //if yes, enter this fork,
+  //cause index.js to be executed again but in child mode.
+  cluster.fork();
+}
+else {
+  //if not, this is a child. this will act liek a server
+  //and nothing else.
+  const express = require('express');
+  const app = express();
 
-app.listen(3000);
+  function doWork(duration){
+    //uses maximum CPU during the duration input given
+    const start = Date.now();
+
+    while(Date.now() - start < duration){
+
+    }
+  }
+
+  app.get('/', (req,res) => {
+    doWork(5000);
+    res.send('Hi there.');
+  });
+
+  app.listen(3000);
+}
